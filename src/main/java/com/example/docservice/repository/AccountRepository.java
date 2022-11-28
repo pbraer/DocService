e6412923c350c4ad2c94b6f8003b96b4d9500338
krendelek42 <k.dokukina@g.nsu.ru>
2022-11-28 17:26:40 +0700

    Added interface AccountRepository in repository, which interact with DataBase

--- src/main/java/com/example/docservice/repository/AccountRepository.java
+++ src/main/java/com/example/docservice/repository/AccountRepository.java
@@ -1,4 +1,14 @@
 package com.example.docservice.repository;
+import com.example.docservice.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface AccountRepository {
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID>{
+    @Query(value = "SELECT * FROM doctors", nativeQuery = true)
+    List<Account> findAllDoctors();
 }
