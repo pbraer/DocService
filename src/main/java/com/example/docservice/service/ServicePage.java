aa14fc8a44eb1d37beff63ba1a4ea2c1bf1c9366
memareve <yqqN87cjbeER>
2022-12-02 16:56:39 +0700

    Chage controller.java for fix White label error /working

--- src/main/java/com/example/docservice/service/ServicePage.java
+++ src/main/java/com/example/docservice/service/ServicePage.java
@@ -2,17 +2,16 @@ package com.example.docservice.service;
 
 import com.example.docservice.dto.Login;
 import com.example.docservice.entity.ProfileDoc;
-/*
+
 @org.springframework.stereotype.Service
 public class ServicePage {
-
 //    private final DocRepository accountRepository;
 //
 //    public ServicePage(DocRepository accountRepository) {
 //        this.accountRepository = accountRepository;
 //    }
 
-    public void createUser(Login login) {
+    public static void createUser(Login login) {
         ProfileDoc profileDoc = new ProfileDoc();
         profileDoc.setEmail(login.getLogin());
         profileDoc.setPassword(login.getPass());
@@ -24,4 +23,3 @@ public class ServicePage {
     }
 }
 
- */
