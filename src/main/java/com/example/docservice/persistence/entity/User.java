4741d320a67b3d8b57f20dc521bee39c5633f0b8
pbraer <braer.a.s@gmail.com>
2022-12-06 15:33:35 +0700

    Role & User entity

--- src/main/java/com/example/docservice/persistence/entity/User.java
+++ src/main/java/com/example/docservice/persistence/entity/User.java
@@ -1,4 +1,55 @@
 package com.example.docservice.persistence.entity;
 
-public class User {
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "usertable")
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Size(min=1, message = "Введите почту")
+    private String username;
+    @Size(min=1, message = "Введите пароль")
+    private String password;
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 }
