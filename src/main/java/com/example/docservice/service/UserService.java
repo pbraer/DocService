77fcd7fdbac7bba789b5f9e43db40ce1885575cd
pbraer <braer.a.s@gmail.com>
2022-12-12 16:01:00 +0700

    start making personal page profile

--- src/main/java/com/example/docservice/service/UserService.java
+++ src/main/java/com/example/docservice/service/UserService.java
@@ -5,12 +5,11 @@ import com.example.docservice.dto.Login;
 import com.example.docservice.persistence.entity.User;
 import com.example.docservice.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+
 public class UserService {
     @Autowired
     private UserRepository userRepository;
